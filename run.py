37c60d9942fe1a50c81d6b183f7ae9bb231ab7c4
Rohan Mishra <rohanmishra2017@gmail.com>
2017-11-07 11:53:00 -0800

    Server returns random word

--- run.py
+++ run.py
@@ -1,8 +1,21 @@
 # -*- encoding: utf-8 -*-
 from flask import Flask
+from random import randrange
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return 'Homapage'
+
+@app.route('/random/word')
+def random_word():
+    with open('gangja.txt', 'r') as f:
+        words = f.read()
+
+    words = words.split()
+    random_word_index = randrange(0, len(words))
+    random_word = words[random_word_index]
+    print(random_word)
+    
+    return random_word
 
