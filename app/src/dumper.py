b22429780558e2860d2a7baaa8df357bfa3f2b17
Rohan Mishra <rohanmishra2017@gmail.com>
2017-12-05 23:05:58 -0800

    Removed sys.don't_write_byte_code and started Tweet dumper class

---
+++ app/src/dumper.py
@@ -0,0 +1,63 @@
+# -*- encoding: utf-8 -*-
+"""
+dumper.py
+
+This module uses Twitter's Tweepy library to create aCSV of a user's tweets.
+"""
+
+import os
+import csv
+import tweepy
+
+class Dumper(object):
+
+    def __init__(self, handle):
+        self.handle = handle
+        self.api = self.authorize()
+
+    def authorize(self):
+        consumer_key = os.environ.get("CONSUMER_KEY")
+        consumer_secret = os.environ.get("CONSUMER_SECRET")
+
+        # authorize Twitter
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+        # intalize Tweepy
+        return tweepy.API(auth)
+
+    def get_all_tweets(self):
+        tweets = []
+
+        # can request 200 tweets at a time
+        new_tweets = self.api.user_timeline(screen_name = self.handle, count = 200)
+        tweets.extend(new_tweets)
+
+        # oldest tweet, subtract one
+        oldest_tweet = tweets[-1].id - 1
+
+        # get tweets until maxed out
+        while len(new_tweets) > 0:
+            print("Getting tweets before %s" % oldest)
+            
+            # use oldest tweet
+            new_tweets = self.api.user_timeline(screen_name = self.handle, 
+                                                count = 200, max_id = oldest)
+
+            tweets.extend(new_tweets)
+
+            # update id
+            oldest = tweets[-1].id - 1
+
+            print("... %s tweets downloaded so far" % (len(tweets)))
+
+
+if __name__ == "__main__":
+    """ Get Twitter credentials on server start """
+    
+    key = input("ENTER CONSUMER KEY: ")
+    secret = input("ENTER CONSUMER SECRET: ")
+
+    os.environ["CONSUMER_KEY"] = key
+    os.environ["CONSUMER_SECRET"] = secret
+    
+
--- run.py
+++ run.py
@@ -14,5 +14,3 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host = "0.0.0.0", port = port, use_reloader = True) # TODO - remove use_reloader?
 
-    sys.dont_write_bytecode = True
-
